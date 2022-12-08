ee57eeeddd3940bf711ff7df17e0c307dcaf9ae9
James <knotsite@gmail.com>
2022-12-08 20:48:37 +0700

    init code

---
+++ src/pages/product-detail.tsx
@@ -0,0 +1,21 @@
+export default function ProductDetailPage() {
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+      <hr />
+      <div>
+        <div>Product Name: Apple</div>
+        <div>
+          Product Description: Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Qui veritatis nihil consequuntur officia explicabo
+          assumenda voluptates necessitatibus ipsum. Quia a, maiores odio
+          dolorem deleniti eum reprehenderit alias officia omnis obcaecati.
+        </div>
+        <div>Product Price: $2100</div>
+        <div>
+          Product Image: <img src="" alt="some product image here" />
+        </div>
+      </div>
+    </div>
+  );
+}
